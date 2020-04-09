d398b54ed3abf77a5577119bebf0ddc53525fb63
dev <svarog96@gmail.com>
2020-04-09 11:48:37 +0500

    sorter: add multithreading test to task1

--- src/ru/tandemservice/test/task1/test/Task1ImplTest.java
+++ src/ru/tandemservice/test/task1/test/Task1ImplTest.java
@@ -11,11 +11,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class Task1ImplTest {
-
+    private final IStringRowsListSorter task1Impl = Task1Impl.INSTANCE;
     @Test
     public void sortNumbersTest() {
-        IStringRowsListSorter task1Impl = Task1Impl.INSTANCE;
-
         String[] testData1 = { "1" };
         String[] testData2 = { "2" };
         String[] testData3 = { "3" };
@@ -36,8 +34,6 @@ public class Task1ImplTest {
 
     @Test
     public void sortNumbersNullAndSpaceTest() {
-        IStringRowsListSorter task1Impl = Task1Impl.INSTANCE;
-
         String[] testData1 = { "1" };
         String[] testData2 = { "2" };
         String[] testData3 = { "3" };
@@ -64,7 +60,6 @@ public class Task1ImplTest {
 
     @Test
     public void sortNullAndStringsTest() {
-        IStringRowsListSorter task1Impl = Task1Impl.INSTANCE;
         String[] testDataA = {"A_null", "А", "А", null };
         String[] testDataB = {"B_null", "B", "B", null };
         String[] testDataC = {"1ram16gb256", "C", "C", "1ram16gb256" };
@@ -86,7 +81,6 @@ public class Task1ImplTest {
 
     @Test
     public void sortNullAndEmptyStringsTest() {
-        IStringRowsListSorter task1Impl = Task1Impl.INSTANCE;
         String[] testDataA = {"A_null", null };
         String[] testDataB = {"", "" };
         String[] testDataC = {"C_null", null };
@@ -108,7 +102,6 @@ public class Task1ImplTest {
 
     @Test
     public void sortByNumbersInSubStringTest() {
-        IStringRowsListSorter task1Impl = Task1Impl.INSTANCE;
         String[] testData1 = {"1", "!@#$% ^&*()_+1 SDSFS"};
         String[] testData2 = {"2", "!@#$% ^&*()_+2 FEASFASAS"};
         String[] testData3 = {"3", "!@#$% ^&*()_+3ewwerwe"};
@@ -130,7 +123,6 @@ public class Task1ImplTest {
 
     @Test
     public void sortAllAsStringsTest() {
-        IStringRowsListSorter task1Impl = Task1Impl.INSTANCE;
         String[] testData1 = {"1", "a123"};
         String[] testData2 = {"2", "123a"};
         String[] testData3 = {"3", "!b123a"};
@@ -148,7 +140,6 @@ public class Task1ImplTest {
 
     @Test
     public void sortSimilarStringsTest() {
-        IStringRowsListSorter task1Impl = Task1Impl.INSTANCE;
         String[] testData1 = {"1", "b"};
         String[] testData2 = {"2", "bac"};
         String[] testData3 = {"3", "ba"};
@@ -166,7 +157,6 @@ public class Task1ImplTest {
 
     @Test
     public void sortDiferentSizeStringsTest() {
-        IStringRowsListSorter task1Impl = Task1Impl.INSTANCE;
         String[] testData1 = {"1", "2a"};
         String[] testData2 = {"2", "2a2"};
         String[] testData3 = {"3", "2a5"};
@@ -184,7 +174,6 @@ public class Task1ImplTest {
 
     @Test
     public void globalSortingTest() {
-        IStringRowsListSorter task1Impl = Task1Impl.INSTANCE;
         String[] testDataA = {"null", "A", "A", null, "A"};
         String[] testDataB = {"null", "B", "B", null, "B"};
         String[] testDataC = {"", "C", "C", "", "C"};
@@ -227,6 +216,68 @@ public class Task1ImplTest {
         assertEquals("1ram16gb256", testData.get(8)[0]);
         assertEquals("ПГП-12-1", testData.get(9)[0]);
         assertEquals("ПГП-12-3", testData.get(10)[0]);
+    }
+
+    @Test
+    public void multiThreadingTest() {
+        String[] testDataA = {"null", "A", "A", null, "A"};
+        String[] testDataB = {"null", "B", "B", null, "B"};
+        String[] testDataC = {"", "C", "C", "", "C"};
+        String[] testDataD = {"", "D", "D", "", "D"};
+        String[] testDataE = {" ", "E", "E", " ", "E"};
+        String[] testDataF = {"1ram16gb256", "F", "F", "1ram16gb256", "F"};
+        String[] testDataG = {"!rpg-7", "G", "G", "!rpg-7", "G"};
+        String[] testDataH = {"ПГП-12-1", "H", "H", "ПГП-12-1", "H"};
+        String[] testDataJ = {"&ПГП-12-2", "J", "J", "&ПГП-12-2", "J"};
+        String[] testDataX = {"ПГП-12-3", "X", "X", "ПГП-12-3", "X"};
+        String[] testDataY = {" ПГ-12", "Y", "Y", " ПГ-12", "Y"};
+
+        String[] testData1 = {"1", "!@#$% ^&*()_+1 SDSFS"};
+        String[] testData2 = {"2", "!@#$% ^&*()_+2 FEASFASAS"};
+        String[] testData3 = {"3", "!@#$% ^&*()_+3ewwerwe"};
+        String[] testData4 = {"4", "!@#$% ^&*()_+4EDGGsew"};
+
+        List<String[]> testDataNum = Arrays.asList(testData2, testData4, testData1, testData3);
+
+        List<String[]> testDataLetter = Arrays.asList(testDataH, testDataA, testDataB, testDataY, testDataF,
+                testDataC, testDataJ, testDataX, testDataE, testDataG, testDataD
+        );
+
+        Runnable run1 = () -> task1Impl.sort(testDataLetter, 3);
+        Runnable run2 = () -> task1Impl.sort(testDataNum, 1);
+
+        Thread t1 = new Thread(run1);
+        Thread t2 = new Thread(run2);
+
+        t2.start();
+        t1.start();
+
+
+        try {
+            t2.join();
+            t1.join();
+
+            assertEquals("null", testDataLetter.get(0)[0]);
+            assertEquals("null", testDataLetter.get(1)[0]);
+            assertEquals("", testDataLetter.get(2)[0]);
+            assertEquals("", testDataLetter.get(3)[0]);
+            assertEquals(" ", testDataLetter.get(4)[0]);
+            assertEquals(" ПГ-12", testDataLetter.get(5)[0]);
+            assertEquals("!rpg-7", testDataLetter.get(6)[0]);
+            assertEquals("&ПГП-12-2", testDataLetter.get(7)[0]);
+            assertEquals("1ram16gb256", testDataLetter.get(8)[0]);
+            assertEquals("ПГП-12-1", testDataLetter.get(9)[0]);
+            assertEquals("ПГП-12-3", testDataLetter.get(10)[0]);
+
+            assertEquals("!@#$% ^&*()_+1 SDSFS", testDataNum.get(0)[1]);
+            assertEquals("!@#$% ^&*()_+2 FEASFASAS", testDataNum.get(1)[1]);
+            assertEquals("!@#$% ^&*()_+3ewwerwe", testDataNum.get(2)[1]);
+            assertEquals("!@#$% ^&*()_+4EDGGsew", testDataNum.get(3)[1]);
+            assertEquals("!@#$% ^&*()_+4EDGGsew", testDataNum.get(3)[1]);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 }
